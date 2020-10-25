bdf54f1a75d9bffceae28b7db97d3d72e8e60cd6
Andy <alayers2@gmail.com>
2020-10-25 08:19:52 -0500

    done testing accounts context

--- apps/cronitex/test/cronitex/accounts_test.exs
+++ apps/cronitex/test/cronitex/accounts_test.exs
@@ -36,6 +36,17 @@ defmodule Cronitex.AccountsTest do
     assert 1 = Enum.count(Accounts.list_users())
   end
 
+  test "list by ID should filter users" do
+    user1 = TestHelpers.user_fixture()
+    user2 = TestHelpers.user_fixture()
+
+    filtered_users = Accounts.list_users_with_ids([user1.id])
+    assert 1 = Enum.count(filtered_users)
+
+    filtered_users = Accounts.list_users_with_ids([user1.id, user2.id])
+    assert 2 = Enum.count(filtered_users)
+  end
+
   test "we can get a user by id" do
     user = TestHelpers.user_fixture()
     assert user = Accounts.get_user!(user.id)
@@ -53,7 +64,6 @@ defmodule Cronitex.AccountsTest do
 
   test "unique constraint on usernames" do
     assert {:ok, user} = Accounts.register_user(@valid_attrs)
-    IO.inspect(user)
     assert {:error, changeset} = Accounts.register_user(@valid_attrs)
     assert %{username: ["has already been taken"]} = errors_on(changeset)
   end
@@ -73,5 +83,26 @@ defmodule Cronitex.AccountsTest do
     catch_error Accounts.get_user_by!(username: "blah")
   end
 
+  test "we can authenticate a valid user" do
+    user = TestHelpers.user_fixture()
+
+    assert {:ok, user} = Accounts.authenticate_by_username_and_pass(
+      user.username, user.password
+    )
+  end
+
+  test "unauthorized returned for bad pasword" do
+    user = TestHelpers.user_fixture()
+
+    assert {:error, :unauthorized} = Accounts.authenticate_by_username_and_pass(
+      user.username, "bad_pass"
+    )
+  end
+
+  test "not found for unknown username" do
+    assert {:error, :not_found} = Accounts.authenticate_by_username_and_pass(
+      "bad_username", "bad_pass"
+    )
+  end
 
 end
