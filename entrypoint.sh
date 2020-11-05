9e0ce3c8aaaef73d8ada1c8871d0f41b827672f3
Andy <alayers2@gmail.com>
2020-11-05 09:39:16 -0600

    update entrypoint to do npm install after volume is mounted

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,6 +18,10 @@ if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
   echo "Database $PGDATABASE created."
 fi
 
+
+cd /app/apps/cronitex_web/assets && npm install
+cd /app
+
 # exec iex --cookie secret --name cronitex@docker \
 #     --erl '-kernel inet_dist_listen_min 9000'  \
 #     --erl '-kernel inet_dist_listen_max 9000' \
